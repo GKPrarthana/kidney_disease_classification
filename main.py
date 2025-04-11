98aafb71a7e72b652fae3c032c0eee897137df61
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-04-11 21:30:48 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+STAGE_NAME = "Prepare base model"
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
