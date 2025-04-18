9f451201bf724c84c044769336f6b620ab29e174
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-04-18 23:18:47 +0530

    training updated

--- src/cnnClassifier/components/model_training.py
+++ src/cnnClassifier/components/model_training.py
@@ -1,39 +1,96 @@
-from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
+import os
 import tensorflow as tf
-from cnnClassifier.entity.config_entity import TrainingConfig
+tf.config.run_functions_eagerly(True)  # ✅ This enables eager execution
 
-class ConfigurationManager:
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
+from zipfile import ZipFile
+import urllib.request as request
+import time
 
-        self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filepath)
 
-        create_directories([self.config.artifacts_root])
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+
+    
+    def get_base_model(self):
+        self.model = tf.keras.models.load_model(
+            self.config.updated_base_model_path,
+            compile=False  # 👈 load model without attached optimizer
+        )
+
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss="categorical_crossentropy",
+            metrics=["accuracy"]
+        )
+
+
+    def train_valid_generator(self):
+
+        datagenerator_kwargs = dict(
+            rescale = 1./255,
+            validation_split=0.20
+        )
+
+        dataflow_kwargs = dict(
+            target_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            interpolation="bilinear"
+        )
+
+        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+            **datagenerator_kwargs
+        )
+
+        self.valid_generator = valid_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="validation",
+            shuffle=False,
+            **dataflow_kwargs
+        )
+
+        if self.config.params_is_augmentation:
+            train_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+                rotation_range=40,
+                horizontal_flip=True,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                **datagenerator_kwargs
+            )
+        else:
+            train_datagenerator = valid_datagenerator
+
+        self.train_generator = train_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="training",
+            shuffle=True,
+            **dataflow_kwargs
+        )
+
+    
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
+
 
 
     
-    def get_training_config(self) -> TrainingConfig:
-        training = self.config.training
-        prepare_base_model = self.config.prepare_base_model
-        params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-images")
-        create_directories([
-            Path(training.root_dir)
-        ])
+    def train(self):
+        self.steps_per_epoch = self.train_generator.samples // self.train_generator.batch_size
+        self.validation_steps = self.valid_generator.samples // self.valid_generator.batch_size
 
-        training_config = TrainingConfig(
-            root_dir=Path(training.root_dir),
-            trained_model_path=Path(training.trained_model_path),
-            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
-            training_data=Path(training_data),
-            params_epochs=params.EPOCHS,
-            params_batch_size=params.BATCH_SIZE,
-            params_is_augmentation=params.AUGMENTATION,
-            params_image_size=params.IMAGE_SIZE
+        self.model.fit(
+            self.train_generator,
+            epochs=self.config.params_epochs,
+            steps_per_epoch=self.steps_per_epoch,
+            validation_steps=self.validation_steps,
+            validation_data=self.valid_generator
         )
 
-        return training_config
+        self.save_model(
+            path=self.config.trained_model_path,
+            model=self.model
+        )
